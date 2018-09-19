8de3ca298880679bc00dcc7f6159e5a6e2211db0
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-19 19:30:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,15 +18,14 @@ return 'done'
 
 }
 
-function incrementVariable() {
-  i = i + 1;
+
 }
 
 function doWhileLoop(array) {
   
    do { array.shift()
   
-  } while (array.length > 0 && incrementVariable())
+  } while (array.length > 0 /*&& incrementVariable()*/)
   
  return array
 
